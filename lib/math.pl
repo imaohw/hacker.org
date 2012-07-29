9fe14c22b35a5f277455d6f840300eebcfe4d96a
Sebastian Köhler <sebkoehler@whoami.org.uk>
2012-07-29 23:30:22 +0200

    095

--- lib/math.pl
+++ lib/math.pl
@@ -45,4 +45,50 @@ sub fibonacci {
     return \@fib;
 }
 
+sub largest_prime {
+    my $n = shift;
+
+    my $i;
+    my $factor;
+    
+    if(abs($n)<=1) {
+        return $n;
+    }
+    
+    if($n % 2 == 0) {
+        $factor = 2;
+
+        while($n % $factor == 0) {
+            $n /= $factor;
+        }
+    }
+    
+    if($n % 3 == 0) {
+        $factor = 3;
+
+        while($n % $factor == 0) {
+            $n /= $factor;
+        }
+    }
+
+    $factor = 5;
+    while($n > $factor) {
+        print "$factor\n";
+        while($n % $factor == 0) {
+            $n /= $factor;
+        }
+        $factor += 2;
+        
+        if($factor == $n) {
+            last;
+        }
+
+        while($n % $factor == 0) {
+            $n /= $factor;
+        }
+        $factor += 4;
+    }   
+    
+    return $factor;
+}
 1;
