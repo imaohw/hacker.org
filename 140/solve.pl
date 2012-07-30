6413e867dea4ccdf2fdbd048c1d6a0d4e44db8ec
Sebastian Köhler <sebkoehler@whoami.org.uk>
2012-07-30 22:34:18 +0200

    140

---
+++ 140/solve.pl
@@ -0,0 +1,14 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+open(FILE,"text.txt") or die($!);
+    my $text = do { local $/; <FILE>};
+close(FILE);
+
+my $cipher = join('',($text =~ /[A-Z]/g));
+$cipher =~ tr/ABCDEFGHIJKLMNOPQRSTUVWXYZ/AHWREFEHSJKVMNOPIASTUVNTYZ/;
+
+print $cipher;
+print "\n";
