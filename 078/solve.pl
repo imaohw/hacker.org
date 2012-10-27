8ff7f9fc32d711159cc41d753fab5fc0d4f90ac7
Sebastian Köhler <sebkoehler@whoami.org.uk>
2012-10-28 00:25:56 +0200

    078

--- 078/solve.pl
+++ 078/solve.pl
@@ -6,36 +6,10 @@ use warnings;
 use LWP;
 use URI::Escape;
 
-my $username = "whoami";
-my $password = "hund1234";
+require "../lib/account.pl"
 
-my $browser = LWP::UserAgent->new;
-$browser->cookie_jar({});
-
-sub login {
-    my $url = 'http://www.hacker.org/forum/login.php';
-    my $response = $browser->post($url,
-        [ username => "$username", 
-          password => "$password",
-          redirect => '',
-          login => 'Log In'
-        ]);
-    if($response->code == 302) {
-        $response = $browser->get('http://www.hacker.org');
-        if($response->content =~ /$username<\/small>/) {
-            print "Successful login\n";
-            return 0;
-        } else {
-            print "Error on login\n";
-            return 1;
-        }
-    } else {
-        print "Error on login\n";
-        return 1;
-    }
-}
-
-if(login == 0) {
+my $browser = login("xxx", "xxx");
+if(browser) {
     my $url = "http://www.hacker.org/challenge/chal.php";
     my $response = $browser->get($url."?id=78");
     
