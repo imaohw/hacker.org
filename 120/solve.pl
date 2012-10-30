2027994b4f415ff75f602944ea72d16cfc0205b4
Sebastian Köhler <sebkoehler@whoami.org.uk>
2012-10-30 15:17:16 +0100

    120

---
+++ 120/solve.pl
@@ -0,0 +1,13 @@
+#!/usr/bin/perl 
+
+# Copyright (c) 2012, Sebastian Köhler<sebkoehler@whoami.org.uk>
+
+use strict;
+use warnings;
+
+use bignum;
+
+my $phi = (1 + sqrt(5)) / 2;
+my $beta = -1/$phi;
+
+printf("%.1f\n", 150000000000000 * log($phi) - log($phi-$beta));
