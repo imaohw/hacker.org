d686d90f8e2fd4f1541d71731a450d6b7c11b42a
Sebastian Köhler <sebkoehler@whoami.org.uk>
2013-03-21 14:05:01 +0100

    275

---
+++ 275/solve.pl
@@ -0,0 +1,9 @@
+#!/usr/bin/env perl 
+
+use strict;
+use warnings;
+
+my $crypt = "tulgqBmqBvuqbdhpslBtuclBmpBjpBfuzclstBjgsCBuztBxhtjBmpBvpfgzepBjpBbpctBdpgccqBehfk.BhBxpstBtuBjheBjuzepBgsqxgqBtuBezdodhepBjhmBxhtjBjumpmglpBeuzo.BhBxgckBhsBtuBjheBduumBuscqBtuBbhslBjhmBjuukhsCBzoBxhtjBmqBehetpd.BejpBfgstBldhap.BuzdBmumBlduapBjpdBtjpdp.Bbmc";
+
+$crypt =~ tr/BulgqvbdhpscjfzCxseoa/ odaybfrienlhcugwespv/;
+print "$crypt\n";
