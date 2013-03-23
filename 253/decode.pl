25c6fbb3c09ed96e014df07b67e4982228506d4b
Sebastian Köhler <sebkoehler@whoami.org.uk>
2013-03-23 12:40:54 +0100

    253

---
+++ 253/decode.pl
@@ -0,0 +1,9 @@
+#!/usr/bin/env perl 
+
+use strict;
+use warnings;
+
+use MIME::Base64 qw(decode_base64);
+
+
+print decode_base64("4uTX1eTV2NWxw9Wl0tTHxmHzyGTB9OfmQkNk89Hz+LTx4rjXoqMAAKO0paHh0qFB88a3xuKmEA==");
